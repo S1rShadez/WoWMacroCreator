63b3e5413799fc7e6eba64dba42845d196a8817a
S1rShadez <arthure97@hotmail.com>
2024-01-26 14:00:43 +0100

    Started working on the macro builder for combat commands

---
+++ WoWSoDMacroCreator/CombatCommands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WoWSoDMacroCreator.Program;
+
+namespace WoWSoDMacroCreator
+{
+    public class CombatCommands
+    {
+        static string SelectMechanic()
+        {
+            string text = "";
+            while (true)
+            {
+                switch (Ask("Select combat mechanic:\n1. Cast spell/ability"))
+                {
+                    case "1":
+                        text += "/cast";
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option selected");
+                        Continue();
+                        Console.Clear();
+                        continue;
+                }
+                break;
+            }
+
+            return text;
+        }
+
+        static string AddModifier()
+        {
+            string text = "[";
+            switch(Ask("Who do you want to target?\n1. Ally\n2. Enemy\n3. Self"))
+            {
+                case "1":
+                    if(Ask("Is the friendly alive? y/n").ToLower() == "y")
+                    {
+                        text += "help, nodead";
+                    }
+                    else
+                    {
+                        text += "help, dead";
+                    }
+                    break;
+            }
+
+            return text;
+        }
+    }
+}
---
+++ WoWSoDMacroCreator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWSoDMacroCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the WoW Classic macro creator!");
+
+            while (true)
+            {
+                switch (Ask("What would you like to do?\n1. Create character macro\n2. Create combat macro"))
+                {
+                    case "1":
+                        Console.WriteLine("Tests");
+                        break;
+                    case "2":
+                        Console.WriteLine("Test 2");
+                        break;
+                    default:
+                        Console.WriteLine("Unrecognized input, try again");
+                        Continue();
+                        Console.Clear();
+                        continue;
+                }
+                break;
+            }
+        }
+
+        public static string Ask(string s)
+        {
+            Console.WriteLine(s);
+            return Console.ReadLine();
+        }
+
+        public static void Continue()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
